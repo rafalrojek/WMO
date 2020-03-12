debfed9e05c40363d6558ba5f8cdb3b041cddcd0
Rafał <rafalrojek96@gmail.com>
2020-03-12 14:44:00 +0100

    Modified GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
 
