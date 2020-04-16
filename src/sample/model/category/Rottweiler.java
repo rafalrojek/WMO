e77d227062757a3b471104ab253c0890af0c44d9
Rafał <rafalrojek96@gmail.com>
2020-04-16 11:55:09 +0200

    Edit rottweiler

--- src/sample/model/category/Rottweiler.java
+++ src/sample/model/category/Rottweiler.java
@@ -10,13 +10,13 @@ public class Rottweiler extends Category {
     public Rottweiler() {
         nameOfCategory = CategoriesEnum.ROTTWEILER;
         addFactor(PROJECT_SIZE, 0.2, ALOT)
-                .addFactor(TEAM_SIZE, 0.2, MEDIUM,ALOT)
-                .addFactor(TIME, 0.1, MEDIUM,ALOT)
-                .addFactor(FIELD_KNOWLEDGE, 0.1, LITTLE,MEDIUM)
-                .addFactor(FORMALIZATION_LEVEL, 0.1, ALOT)
-                .addFactor(INNOVATION_LEVEL, 0.1, MEDIUM,ALOT)
-                .addFactor(DEVELOPERS_EXP, 0.1, ALOT)
-                .addFactor(QUALITY, 0.1, ALOT)
-                .addFactor(STABILITY, 0.2, YES);
+                .addFactor(TEAM_SIZE, 0.05, MEDIUM,ALOT)
+                .addFactor(TIME, 0.05, MEDIUM,ALOT)
+                .addFactor(FIELD_KNOWLEDGE, 0.05, LITTLE,MEDIUM)
+                .addFactor(FORMALIZATION_LEVEL, 0.5, ALOT)
+                .addFactor(INNOVATION_LEVEL, 0.05, MEDIUM,ALOT)
+                .addFactor(DEVELOPERS_EXP, 0.05, ALOT)
+                .addFactor(QUALITY, 0.05, ALOT)
+                .addFactor(STABILITY, 0.05, YES);
     }
 }
